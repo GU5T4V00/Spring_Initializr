6e9634936ba29dcbc8d7d6aed7df42bdb89f978a
Gustavo Henrique dos Anjos <142633768+GU5T4V00@users.noreply.github.com>
2025-11-02 15:41:36 -0300

    Update Entity1.java

--- demo/src/main/java/com/java/Spring_Initializr/Entity/Entity1.java
+++ demo/src/main/java/com/java/Spring_Initializr/Entity/Entity1.java
@@ -1,10 +1,12 @@
 package com.java.Spring_Initializr.Entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
 @Table(name = "entity_1")
