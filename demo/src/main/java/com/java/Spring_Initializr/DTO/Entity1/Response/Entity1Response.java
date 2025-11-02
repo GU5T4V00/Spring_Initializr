ac1921862ceb25a7aea2c5fa282eafa6ddf74311
Gustavo <gu.anjos04@gmail.com>
2025-11-02 11:28:21 -0300

    Att

---
+++ demo/src/main/java/com/java/Spring_Initializr/DTO/Entity1/Response/Entity1Response.java
@@ -0,0 +1,19 @@
+package com.java.Spring_Initializr.DTO.Entity1.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO responsável por representar os dados de saída (Response)
+ * enviados de volta ao cliente.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Entity1Response {
+
+    private Long id;
+    private String name;
+    private Double value;
+}
